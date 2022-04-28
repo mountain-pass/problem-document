b66709c1e93de977d138802edad89effbc2f3fc4
Tom Howard <tom@windyroad.com.au>
2022-04-28 17:27:57 +1000

    build(publish): fixing NPM publishing

--- .projenrc.js
+++ .projenrc.js
@@ -8,7 +8,7 @@ const project = new typescript.TypeScriptProject({
   deps: [], /* Runtime dependencies of this module. */
   description: 'A basic RFC7807 Problem Document with basic type checking and autocomplete', /* The description is just a string that helps people understand the purpose of the package. */
   devDeps: ['http-status-codes', '@types/http-status-codes', 'husky', '@commitlint/config-conventional', '@commitlint/cli'], /* Build dependencies for this module. */
-  packageName: '@mountain-pass/problem-document', /* The "name" in package.json. */
+  packageName: '@mountainpass/problem-document', /* The "name" in package.json. */
   npmDistTag: 'latest', /* The npm distribution tag. */
   npmAccess: 'public', /* The npm access level. */
   releaseToNpm: true, /* Whether to release to npm. */
