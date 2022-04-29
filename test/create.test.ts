558960386de11fd09b037aa6f4348fbb7a0166cb
Tom Howard <tom@windyroad.com.au>
2022-04-30 00:26:10 +1000

    docs: added documentation

--- test/create.test.ts
+++ test/create.test.ts
@@ -1,20 +1,25 @@
-import { StatusCodes } from 'http-status-codes';
-import { ProblemDocument } from '../src';
+import { StatusCodes } from "http-status-codes";
+import { ProblemDocument } from "../src";
 
-test('create', () => {
-  const problemDocument = new ProblemDocument({ detail: 'a', instance: 'b', status: 400, title: 'd', type: 'e' });
+test("create", () => {
+  const problemDocument = new ProblemDocument({
+    detail: "a",
+    instance: "b",
+    status: 400,
+    title: "d",
+    type: "e",
+  });
   expect(problemDocument).toBeInstanceOf(ProblemDocument);
   const { detail, instance, status, title, type, ...other } = problemDocument;
-  expect(detail).toBe('a');
-  expect(instance).toBe('b');
+  expect(detail).toBe("a");
+  expect(instance).toBe("b");
   expect(status).toBe(400);
-  expect(title).toBe('d');
-  expect(type).toBe('e');
+  expect(title).toBe("d");
+  expect(type).toBe("e");
   expect(other).toEqual({});
 });
 
-
-test('createEmpty', () => {
+test("createEmpty", () => {
   const problemDocument = new ProblemDocument({});
   expect(problemDocument).toBeInstanceOf(ProblemDocument);
   const { detail, instance, status, title, type, ...other } = problemDocument;
@@ -26,19 +31,26 @@ test('createEmpty', () => {
   expect(other).toEqual({});
 });
 
-test('createWithExtension', () => {
-  const problemDocument = new ProblemDocument({ detail: 'a', instance: 'b', status: StatusCodes.BAD_REQUEST, title: 'd', type: 'e', foo: 'bar' });
+test("createWithExtension", () => {
+  const problemDocument = new ProblemDocument({
+    detail: "a",
+    instance: "b",
+    status: StatusCodes.BAD_REQUEST,
+    title: "d",
+    type: "e",
+    foo: "bar",
+  });
   expect(problemDocument).toBeInstanceOf(ProblemDocument);
   const { detail, instance, status, title, type, ...other } = problemDocument;
-  expect(detail).toBe('a');
-  expect(instance).toBe('b');
-  expect(title).toBe('d');
-  expect(type).toBe('e');
-  expect(other).toEqual({ foo: 'bar' });
+  expect(detail).toBe("a");
+  expect(instance).toBe("b");
+  expect(title).toBe("d");
+  expect(type).toBe("e");
+  expect(other).toEqual({ foo: "bar" });
 });
 
-test('createEmptyWithExtension', () => {
-  const problemDocument = new ProblemDocument({ foo: 'bar' });
+test("createEmptyWithExtension", () => {
+  const problemDocument = new ProblemDocument({ foo: "bar" });
   expect(problemDocument).toBeInstanceOf(ProblemDocument);
   const { detail, instance, status, title, type, ...other } = problemDocument;
   expect(detail).toBeUndefined();
@@ -46,5 +58,5 @@ test('createEmptyWithExtension', () => {
   expect(status).toBeUndefined();
   expect(title).toBeUndefined();
   expect(type).toBeUndefined();
-  expect(other).toEqual({ foo: 'bar' });
+  expect(other).toEqual({ foo: "bar" });
 });
